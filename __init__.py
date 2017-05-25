bb825b85944eae511b52a6cbb13a507a018ee26a
Alexey-t <support@uvviewsoft.com>
2017-05-25 15:23:29 +0300

    new command like ST3 Alt+shhift+w

--- __init__.py
+++ __init__.py
@@ -1,6 +1,45 @@
 from cudatext import *
 
+
+def do_tag_new_or_wrap():
+    """
+    Does what SublimeText does on hotkey Alt+Shift+W:
+    on selection: wrap selection with <p></p> and place 2 selections to rename tag
+    w/o selection: add <p></p> with 2 selections and 2 markers:
+      1st TAB press goes into tag,
+      2nd TAB press goes after tag
+    """
+
+    carets = ed.get_carets()
+    if len(carets)!=1: return msg_status('Need single caret')
+
+    x1, y1, x2, y2 = carets[0]
+
+    sel = ed.get_text_sel()
+    if sel:
+        if (y1, x1)>(y2, x2):
+            x1, y1, x2, y2 = x2, y2, x1, y1
+        text = '<p>%s</p>' % sel
+        ed.replace(x1, y1, x2, y2, text)
+        ed.set_caret(x1+2, y1, x1+1, y1, CARET_SET_ONE)
+        ed.set_caret(x1+len(sel)+6, y1, x1+len(sel)+5, y1, CARET_ADD)
+        msg_status('Wrapped selection into tag')
+    else:
+        ed.insert(x1, y1, '<p></p>')
+        ed.set_caret(x1+2, y1, x1+1, y1, CARET_SET_ONE)
+        ed.set_caret(x1+6, y1, x1+5, y1, CARET_ADD)
+        ed.markers(MARKERS_DELETE_ALL)
+        ed.markers(MARKERS_ADD, x=x1+7, y=y1, tag=2, len_x=0, len_y=0)
+        ed.markers(MARKERS_ADD, x=x1+3, y=y1, tag=1, len_x=0, len_y=0)
+        ed.set_prop(PROP_TAB_COLLECT_MARKERS, True)
+        msg_status('Added new tag')
+
+
+
 def do_tag(tag):
+    """
+    For selection only: wraps selection into tag: <tag>selected</tag>
+    """
     s = ed.get_text_sel()
     if not s: return msg_status('Text not selected')
     carets = ed.get_carets()
@@ -11,8 +50,7 @@ def do_tag(tag):
         x1, y1, x2, y2 = x2, y2, x1, y1
 
     text = '<%s>%s</%s>' % (tag, s, tag)
-    ed.delete(x1, y1, x2, y2)
-    ed.insert(x1, y1, text)
+    ed.replace(x1, y1, x2, y2, text)
     ed.set_caret(x1, y1, x1+len(text), y1)
     msg_status('Added tag <%s>'%tag)
 
@@ -20,7 +58,12 @@ def do_tag(tag):
 class Command:
     def do_b(self):
         do_tag('b')
+
     def do_i(self):
         do_tag('i')
+
     def do_u(self):
         do_tag('u')
+
+    def tag_new_or_wrap(self):
+        do_tag_new_or_wrap()
--- readme/readme.txt
+++ readme/readme.txt
@@ -1,6 +1,15 @@
 plugin for CudaText.
-gives commands to add HTML tags to selection, e.g.
-  text -->  <b>text</b>
+gives commands:
 
-license: MIT
+1) wrap selection in HTML tag: <b>, <i>, <u>
+  text -->  <b>text</b>
+  
+2) command, to do the same as Sublime Text does on hotkey Alt+Shift+W:
+    on selection: wrap selection with <p></p> and place 2 selections to rename tag
+    w/o selection: add <p></p> with 2 selections and 2 markers:
+      1st TAB press goes into tag,
+      2nd TAB press goes after tag
+  
+
 author: Alexey (CudaText)
+license: MIT
