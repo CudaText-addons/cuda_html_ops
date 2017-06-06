5c13ebf393a73ea4987596334bcfc2e8face3468
BkUser <hhhhhhhhnn70@gmail.com>
2017-06-06 22:24:20 +0300

    commd to convert px-rem

--- __init__.py
+++ __init__.py
@@ -55,6 +55,48 @@ def do_tag(tag):
     msg_status('Added tag <%s>'%tag)
 
 
+def _convert_px_rem(s):
+    s = s.lower()
+    if "rem" in s:
+        s = s.replace("rem", "")
+        n = round(float(s) * 16, 4)
+        return str(n) + 'px'
+
+    if "px" in s:
+        s = s.replace("px", "")
+        n = round(float(s) / 16, 4)
+        return str(n) + 'rem'
+
+
+def do_convert_px_rem():
+    """
+    Takes selection, changes it if it's with "px" or "rem",
+    puts new text back
+    """
+    s = ed.get_text_sel()
+    if not s: return
+
+    if '\n' in s or ' ' in s:
+        msg_status('Cannot convert selection')
+        return
+
+    carets = ed.get_carets()
+    if len(carets)>1:
+        msg_status('Need single caret')
+        return
+
+    x1, y1, x2, y2 = carets[0]
+    if (y1, x1)>(y2, x2):
+        x1, y1, x2, y2 = x2, y2, x1, y1
+
+    s = _convert_px_rem(s)
+    if not s: return
+
+    ed.replace(x1, y1, x2, y2, s)
+    ed.set_caret(x1+len(s), y1, x1, y1)
+    msg_status('Converted px<->rem')
+
+
 class Command:
     def do_b(self):
         do_tag('b')
@@ -67,3 +109,6 @@ class Command:
 
     def tag_new_or_wrap(self):
         do_tag_new_or_wrap()
+
+    def convert_px_rem(self):
+        do_convert_px_rem()
--- readme/readme.txt
+++ readme/readme.txt
@@ -1,15 +1,21 @@
 plugin for CudaText.
-gives commands:
+gives commands for HTML/CSS work (lexer name can be any).
 
-1) wrap selection in HTML tag: <b>, <i>, <u>
-  text -->  <b>text</b>
+1) commands to wrap selection in HTML tag: <b>, <i>, <u>.
+  for ex, "text" ->  "<b>text</b>"
   
 2) command, to do the same as Sublime Text does on hotkey Alt+Shift+W:
     on selection: wrap selection with <p></p> and place 2 selections to rename tag
     w/o selection: add <p></p> with 2 selections and 2 markers:
       1st TAB press goes into tag,
       2nd TAB press goes after tag
-  
+
+3) command, which converts selected value between "px" and "rem" units.
+  it works only if selection is number + "px" or "rem", and changes selection
+  between "px" and "rem" value. it leaves only 4 digits of float-number after dot.
+  for ex, 10px -> 0.625rem -> 10.0px.
+  idea: user "nmsalinas", site: www.nevermindhttp.com
+    
 
 author: Alexey (CudaText)
 license: MIT
