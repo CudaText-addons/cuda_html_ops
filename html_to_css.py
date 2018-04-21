44ae055415b971a6012cf8e4ff8f5456371ae488
Alexey-t <support@uvviewsoft.com>
2018-04-21 23:10:18 +0300

    improve pref dif

--- html_to_css.py
+++ html_to_css.py
@@ -12,6 +12,11 @@ def do_html_to_css_clipboard(compact):
     res = re.findall(REGEX1, text, 0) + \
           re.findall(REGEX2, text, 0)
     res = sorted(list(set(res)))
+    
+    if not res:
+        msg_status('No CSS classes found')
+        app_proc(PROC_SET_CLIP, '')
+        return
 
     text_in = '  ' if compact else '\n\n'
     out = ['/* CSS styles */', ''] + \
@@ -19,4 +24,4 @@ def do_html_to_css_clipboard(compact):
 
     text = '\n'.join(out)
     app_proc(PROC_SET_CLIP, text)
-    msg_status('CSS styles copied to clipboard')
+    msg_status('CSS styles ({}) copied to clipboard'.format(len(res)) )
