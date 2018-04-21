6f94173e193150358869df6f2c59590a2a82d24e
Alexey-t <support@uvviewsoft.com>
2018-04-21 23:15:55 +0300

    fix

--- html_to_css.py
+++ html_to_css.py
@@ -9,10 +9,9 @@ def do_html_to_css_clipboard(compact):
     text = ed.get_text_sel()
     if not text: return
 
-    res = re.findall(REGEX1, text, 0) + \
-          re.findall(REGEX2, text, 0)
+    res = re.findall(REGEX1, text, 0) + re.findall(REGEX2, text, 0)
     res = sorted(list(set(res)))
-    
+
     if not res:
         msg_status('No CSS classes found')
         app_proc(PROC_SET_CLIP, '')
@@ -21,6 +20,6 @@ def do_html_to_css_clipboard(compact):
     text_in = '  ' if compact else '\n\n'
     out = ['.'+name+' {'+text_in+'}\n' for name in res]
     text = '\n'.join(out)+'\n'
-    
+
     app_proc(PROC_SET_CLIP, text)
     msg_status('CSS styles ({}) copied to clipboard'.format(len(res)) )
